c709ae9c2aae392498d9c44dac375175db0481d0
Kevin Centeno <KevinCenteno_90@yahoo.com>
2022-01-29 10:45:36 -0500

    loops

---
+++ JS/loopChallenges/loop.js
@@ -0,0 +1,16 @@
+// print odds 1-20
+
+for(var i = 0; i <= 20; i++){
+    if(i%2 === 1)
+    console.log(i);
+}
+
+// Decreasing Multiples of 3
+
+for(var i = 100; i > 0; i-=3){
+    if( i%3 === 1)
+    console.log(i);
+}
+
+// print the sequence
+
