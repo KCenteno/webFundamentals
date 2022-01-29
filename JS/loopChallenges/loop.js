b4ba23b6f52eef995dc8c1b0b319f4391a8f80af
Kevin Centeno <KevinCenteno_90@yahoo.com>
2022-01-29 10:57:07 -0500

    Lchallenges

--- JS/loopChallenges/loop.js
+++ JS/loopChallenges/loop.js
@@ -14,3 +14,28 @@ for(var i = 100; i > 0; i-=3){
 
 // print the sequence
 
+var array = [4, 2.5, 1, -0.5, -2, -3.5];
+
+for( var i = 0; i < array.length; i++){
+    console.log(array[i])
+}
+
+
+// Sigma
+
+var sum = 0;
+
+for(var i = 0; i <= 100; i++){
+    sum += i
+}
+console.log(sum);
+
+// Factorial
+
+var product = 1;
+
+for(var i = 1; i <= 12; i++){
+    product *= i
+}
+
+console.log(product);
