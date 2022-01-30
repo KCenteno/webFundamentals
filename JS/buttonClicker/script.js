13808dc0acde7dcd713d94453f145687d31283b9
Kevin Centeno <KevinCenteno_90@yahoo.com>
2022-01-29 23:39:38 -0500

    Button Clicker

---
+++ JS/buttonClicker/script.js
@@ -0,0 +1,12 @@
+function hide(element) {
+    element.remove();
+}
+
+
+function turnOff(element) {
+    if(element.innerText == "Login"){
+        element.innerText = "Logout";
+    }else{
+        element.innerText = "Login";
+    }
+}
